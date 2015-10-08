76b79243d2b5f8b3dee5f94f3b31314714151290
byram <natrollus@gmail.com>
2015-10-08 13:28:23 +0300

    toast fonksiyonu eklendi..

--- src/main/java/com/natrollus/klavye/Araclar.java
+++ src/main/java/com/natrollus/klavye/Araclar.java
@@ -4,11 +4,11 @@ import android.content.Context;
 import android.util.Log;
 import android.widget.Toast;
 
-/**
- * Created by byram on 9/28/15.
- */
 public class Araclar {
     public static void logla(Object s){
         Log.v("bilgi",""+s);
+    }
+    public void toast(Context context,String s) {
+        Toast.makeText(context,""+s,Toast.LENGTH_LONG);
     }
 }
