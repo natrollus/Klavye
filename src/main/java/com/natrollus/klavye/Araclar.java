07cb26b11bb800ac0da8feedf59588b92eb260e7
Bayram <natrollus@gmail.com>
2015-10-08 13:25:50 +0300

    Update Araclar.java
    tostla kaldirildi..

--- src/main/java/com/natrollus/klavye/Araclar.java
+++ src/main/java/com/natrollus/klavye/Araclar.java
@@ -11,7 +11,4 @@ public class Araclar {
     public static void logla(Object s){
         Log.v("bilgi",""+s);
     }
-    public static void tostla(Context context,String s) {
-        Toast.makeText(context, "" + s, Toast.LENGTH_LONG).show();
-    }
 }
